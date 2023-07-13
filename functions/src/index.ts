656a754fe02c480e26dfb1e15910d8ad53dc98e7
Aginius <francesco.barabino@tiscali.it>
2023-07-13 19:29:30 +0300

    Fixes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,7 +11,6 @@ function getFoodReward(price: number, foodSales: number, totalSales: number) {
 }
 
 exports.buyFood = functions.https.onRequest(async (req, res) => {
-
     const firestoreInstance = admin.firestore();
     const r = await firestoreInstance.runTransaction(async t => {
         
@@ -42,3 +41,66 @@ exports.buyFood = functions.https.onRequest(async (req, res) => {
 
     res.send({"response": "OK"});
 });
+
+
+export const getDailyTrivia = functions.https.onRequest(async (req, res) => {
+    //return all trivias that are daily
+    try {
+        const trivia = (await db.trivias.where('isDaily', '==', true).limit(1).get()).docs[0];
+        //    return trivia but without isCorrect and comment in answers, and adding doc id to trivia
+        const triviaData = trivia.data();
+
+        const result = {
+            id: trivia.id,
+            question: triviaData.question,
+            answers: triviaData.answers.map((answer: Answer) => {
+                    return answer.text;
+                }
+            ),
+            topicID: triviaData.topicID
+        }
+        res.json(result);
+    } catch (e: any) {
+        res.status(400).json(`Error: ${e.message}`);
+    }
+}
+);
+
+export const submitTriviaAnswer = functions.https.onRequest(async (req, res) => {
+//    received data is {triviaId: string, answerIndex: int}
+
+let triviaId: string;
+let answerIndex: number;
+try {
+    triviaId = req.body.triviaId;
+    answerIndex = req.body.answerIndex;
+} catch (e: any) {
+    res.status(400).json(`Error: invalid request`);
+    return;
+}
+
+try {
+    const trivia = (await db.trivias.doc(triviaId).get()).data();
+    if (!trivia) {
+        res.status(400).json(`Error: Trivia not found`);
+        return;
+    }
+
+    if (trivia.answers[answerIndex].isCorrect) {
+        //    TODO: add points to user
+    }
+
+    const correctAnswerIndex = trivia.answers.findIndex((answer: any) => answer.isCorrect);
+    res.json(
+        {
+            correctAnswerIndex: correctAnswerIndex,
+            comment: trivia.answers[correctAnswerIndex].comment
+        }
+    );
+} catch (e: any) {
+    res.status(400).json(`Error: ${e.message}`);
+}
+});
+
+
+
